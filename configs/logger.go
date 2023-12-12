f8b27834ba965d868d6679af1d67043d2271a27b
tarasusrus <bar.norilsk@gmail.com>
2023-12-12 18:00:24 +0300

    сделал логгер более универсальным, были проблемы в работе с фреймворком и nats

--- configs/logger.go
+++ configs/logger.go
@@ -1,21 +1,40 @@
 package configs
 
 import (
-	"gorm.io/gorm/logger"
 	"log"
 	"os"
-	"time"
 )
 
-func NewLogger() logger.Interface {
-	newLogger := logger.New(
-		log.New(os.Stdout, "/r/n", log.LstdFlags),
-		logger.Config{
-			SlowThreshold:             time.Second,
-			LogLevel:                  logger.Warn,
-			IgnoreRecordNotFoundError: true,
-			ParameterizedQueries:      true,
-			Colorful:                  false,
-		})
-	return newLogger
+// UniversalLogger определяет интерфейс для универсального логгера
+type UniversalLogger interface {
+	Infof(format string, args ...interface{})
+	Warnf(format string, args ...interface{})
+	Errorf(format string, args ...interface{})
+}
+
+// MyLogger является реализацией UniversalLogger
+type MyLogger struct {
+	logger *log.Logger
+}
+
+// NewLogger создает новый экземпляр MyLogger
+func NewLogger() *MyLogger {
+	return &MyLogger{
+		logger: log.New(os.Stdout, "\r\n", log.LstdFlags),
+	}
+}
+
+// Infof реализует логирование уровня Info
+func (l *MyLogger) Infof(format string, args ...interface{}) {
+	l.logger.Printf("INFO: "+format, args...)
+}
+
+// Warnf реализует логирование уровня Warn
+func (l *MyLogger) Warnf(format string, args ...interface{}) {
+	l.logger.Printf("WARN: "+format, args...)
+}
+
+// Errorf реализует логирование уровня Error
+func (l *MyLogger) Errorf(format string, args ...interface{}) {
+	l.logger.Printf("ERROR: "+format, args...)
 }
